bae4b39ca34c99cce3d6211690e5e21eafb7cc33
mishrarohit10 <mishrarohit316x@gmail.com>
2023-05-29 14:52:23 +0530

    first commit

---
+++ helloworld/helloworld.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("lets go" + " ooooooo");
+var employee = "rohit";
+var age = 23;
+var maried = false;
+var anything = "23";
+var salary = 34000.434;
+anything = 45;
---
+++ helloworld/helloworld.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("lets go" + " ooooooo");
+let employee: string = "rohit";
+let age: number = 23;
+let maried: boolean = false;
+let anything: any = "23";
+let salary: number = 34000.434;
+anything = 45;
